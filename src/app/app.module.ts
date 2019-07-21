86bf009864efc27f26b68051e359cb4a65977645
Deymar <herzed@gmail.com>
2019-07-21 16:52:08 -0400

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+/*
+    Modulos
+*/
+import { PagesModule } from './pages/pages.module';
+import { APP_ROUTES } from './app.routes';
+import { RegisterComponent } from './register/register.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
